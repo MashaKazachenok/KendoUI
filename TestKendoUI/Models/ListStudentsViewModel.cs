ca916f9765a0750324f997e193eddb16ee8ebe71
m.kazachenok <kazachenokm@mail.ru>
2015-07-15 08:03:50 +0300

    changed names to tabs

---
+++ TestKendoUI/Models/ListStudentsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestKendoUI.Models
+{
+    public class ListStudentsViewModel
+    {
+        List<string> Students { get; set; }
+    }
+}
